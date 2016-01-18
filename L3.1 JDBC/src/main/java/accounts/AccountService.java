4fd97f60c727a57bc6a5637951cdee9f876a037b
ketaetc <ketaetc@gmail.com>
2016-01-18 23:08:00 +0500

    L3 started

---
+++ L3.1 JDBC/src/main/java/accounts/AccountService.java
@@ -0,0 +1,43 @@
+package accounts;
+
+import dbService.DBException;
+import dbService.DBService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class AccountService {
+
+    private DBService dbService = new DBService();
+
+//    public AccountService() {
+//        DBService dbService = new DBService();
+//    }
+
+    public void addNewUser(UserProfile userProfile) throws DBException {
+        dbService.addUser(userProfile.getLogin(), userProfile.getPass());
+    }
+
+    public UserProfile getUserByLogin(String login) {
+        return loginToProfile.get(login);
+    }
+
+    public UserProfile getUserBySessionId(String sessionId) {
+        return sessionIdToProfile.get(sessionId);
+    }
+
+    public void addSession(String sessionId, UserProfile userProfile) {
+        sessionIdToProfile.put(sessionId, userProfile);
+    }
+
+    public void deleteSession(String sessionId) {
+        sessionIdToProfile.remove(sessionId);
+    }
+}
