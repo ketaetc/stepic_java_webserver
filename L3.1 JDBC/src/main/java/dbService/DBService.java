efbe5c280b48fb4ef384f0e9724ee10aac103ec1
ketaetc <ketaetc@gmail.com>
2016-01-21 02:11:17 +0500

    db file path fix

--- L3.1 JDBC/src/main/java/dbService/DBService.java
+++ L3.1 JDBC/src/main/java/dbService/DBService.java
@@ -81,35 +81,9 @@ public class DBService {
         }
     }
 
-    @SuppressWarnings("UnusedDeclaration")
-    public static Connection getMysqlConnection() {
-        try {
-            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
-
-            StringBuilder url = new StringBuilder();
-
-            url.
-                    append("jdbc:mysql://").        //db type
-                    append("localhost:").           //host name
-                    append("3306/").                //port
-                    append("db_example?").          //db name
-                    append("user=tully&").          //login
-                    append("password=tully");       //password
-
-            System.out.println("URL: " + url + "\n");
-
-            Connection connection = DriverManager.getConnection(url.toString());
-            return connection;
-        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     public static Connection getH2Connection() {
         try {
 
-//            String url = "jdbc:h2:~/IdeaProjects/stepic_java_webserver/L3.1 JDBC/h2db";
             String url = "jdbc:h2:./h2db";
             String name = "test";
             String pass = "test";
